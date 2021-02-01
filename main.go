a7a4889df066b0a43fc97423f3572f3c8bcff7d5
robin019 <liu8613789@gmail.com>
2021-02-02 00:03:32 +0800

    feat: add postgres connection

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "github.com/robin019/url-detection/router"
+import (
+	"github.com/robin019/url-detection/router"
+	_ "github.com/robin019/url-detection/src/psersistence/sql"
+)
 
 func main() {
 	router.Route()
--- src/utils/config/config.go
+++ src/utils/config/config.go
@@ -4,7 +4,7 @@ import (
 	"github.com/kelseyhightower/envconfig"
 )
 
-type LogConfig struct {
+type logConfig struct {
 	File string `required:"true" envconfig:"LOG_FILE"`
 }
 
@@ -12,17 +12,28 @@ type serverConfig struct {
 	Port string `required:"true" envconfig:"SERVER_PORT"`
 }
 
+type databaseConfig struct {
+	User     string `required:"true" envconfig:"DB_USER"`
+	Password string `required:"true" envconfig:"DB_PASSWORD"`
+	Host     string `required:"true" envconfig:"DB_HOST"`
+	Port     string `required:"true" envconfig:"DB_PORT"`
+	DBNAME   string `required:"true" envconfig:"DB_DBNAME"`
+}
+
 var (
-	Log    *LogConfig
-	Server *serverConfig
+	Log      *logConfig
+	Server   *serverConfig
+	Database *databaseConfig
 )
 
 func init() {
-	Log = new(LogConfig)
+	Log = new(logConfig)
 	Server = new(serverConfig)
+	Database = new(databaseConfig)
 
 	//func MustProcess panics if an error occurs. e.g., no such environment variable
 	//for readability, prefixes are not used
 	envconfig.MustProcess("", Log)
 	envconfig.MustProcess("", Server)
+	envconfig.MustProcess("", Database)
 }
