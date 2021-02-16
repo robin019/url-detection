94eada51b34111ca4a9fc2dca77feb3f78fd55b9
robin019 <liu8613789@gmail.com>
2021-02-16 23:23:37 +0800

    fix: fix nginx reverse proxy ip header

--- src/middleware/log.go
+++ src/middleware/log.go
@@ -9,6 +9,7 @@ import (
 func RequestLog() fiber.Handler {
 	return fiberlog.New(fiberlog.Config{
 		TimeFormat: "2006-01-02 15:04:05",
+		Format:     "[${time}] ${header:X-Real-IP} - ${status} - ${latency} ${method} ${path}\n",
 		Output:     logger.RequestLog,
 	})
 }
